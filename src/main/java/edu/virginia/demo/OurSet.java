1ebd23ae4a60421d074954c63f23008ddd3fbf07
Tom Horton <horton.uva@gmail.com>
2019-03-27 13:48:41 -0400

    Copy of files from other project

---
+++ src/main/java/edu/virginia/demo/OurSet.java
@@ -0,0 +1,16 @@
+package edu.virginia.demo;
+
+public class OurSet {
+
+    public boolean contains() {
+        return false;
+    }
+
+    public boolean add(Object o) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("** edu.virginia.demo.OurSet.main(), just for demo purposes.");
+    }
+}
---
+++ src/test/java/edu/virginia/demo/OurSetTest.java
@@ -0,0 +1,23 @@
+package edu.virginia.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OurSetTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void contains() {
+        assertTrue(true);
+    }
+
+    @Test
+    void add() {
+        assertTrue(false);
+    }
+}
